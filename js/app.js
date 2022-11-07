01aad848744cf8d2aebab4ff9c751247a05cb66d
Vlad Vynnyk <vladvynnyk1@gmail.com>
2022-11-07 12:27:32 +0200

    Created lab

---
+++ js/app.js
@@ -0,0 +1,60 @@
+var tab;
+var tabContent;
+
+
+window.onload = function() {
+    tabContent = document.getElementsByClassName('tabContent');
+    tab = document.getElementsByClassName('tab');
+    hideTabsContent(1);
+}
+
+document.getElementById('tabs').onclick = function(event) {
+    var target = event.target;
+    if (target.className == 'tab') {
+        for (var i = 0; i < tab.length; i++) {
+            if (target == tab[i]) {
+                showTabsContent(i);
+                break;
+            }
+        }
+    }
+}
+
+function hideTabsContent(a) {
+    for (var i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
+        tab[i].classList.remove('whiteborder');
+    }
+}
+
+function showTabsContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('whiteborder');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
+
+function fun1() {
+    var rtl = document.getElementById('rtl').value;
+    var rtr = document.getElementById('rtr').value;
+    var rbr = document.getElementById('rbr').value;
+    var rbl = document.getElementById('rbl').value;
+
+    var ttl = document.getElementById('ttl');
+    var ttr = document.getElementById('ttr');
+    var tbr = document.getElementById('tbr');
+    var tbl = document.getElementById('tbl');
+
+    var block = document.getElementById('block');
+
+    ttl.value = rtl;
+    ttr.value = rtr;
+    tbr.value = rbr;
+    tbl.value = rbl;
+
+    block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px ';
+    document.getElementById('output').value = 'border-radius: ' + block.style.borderRadius;
+}
